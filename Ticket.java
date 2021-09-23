051aa2eb7260ffd6f72ae1f5507d3cd742e77c90
devella1 <75735759+devella1@users.noreply.github.com>
2021-09-23 16:50:01 +0530

    Update Ticket.java

--- Ticket.java
+++ Ticket.java
@@ -1,35 +1,55 @@
-public class Ticket.java{
-	public static void main(String[] args) {
-		
+import java.io.*;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Ticket extends Vehicle{
+private final String name;
+private final String registration_number;
+private static long count=0;
+public static String form="F";
+
+ 	public Ticket(String name, String registration_number){
+ 		this.name=name;
+ 		this.registration_number=registration_number;
+ 		count++;
+ 	}
+
+	public String GetName(){
+		return name;
 	}
 
+	public String getSlot_id(){
+		return form;
+	}
 
+	public String getReg_no(){
+		return registration_number;
+	}
 
-	public static void issue_ticket(){
+	// public int issue_time(){
+	// 	return time;
+	// }
 
-		Hashmap <String,Integer> hm = new Hashmap<>();
+	public static void update_info(){
 
-		hm.put("F1SE", 5);
-		hm.put("F2ME", 4);
-		hm.put("F3LE", 3);
-		hm.put("F2SE", 2);
-		hm.put("F1SN", 1);
+		HashMap <String,Integer> hm = new HashMap<>();
 
 
 		// initial values in the hashmap to be filled up by the manager
 		// indices: F1ME thus there will be n*3*2 = 6n keys
 
 
-		String s//has the type: S, M or L.
-		String form="F";
+		String s; //has the type: S, M or L.
 		for(int i=1;i<=n;i++)
 		{
-			form+=i.toString();
+			form+=Integer.toString(i);
 			form+=s;
-			System.out.println(form);
-			if(hm[form])
-				hm.replace("form",hm[form]-1);
+			// System.out.println(form);
+			if(hm.get(form)>0)
+				{
+					hm.replace("form",hm.get(form)-1);
+					break;
+				}
 		}
-
 	}
 }

a96aedd296c16e3e80e8c362bf3b06b09c251d86
Surya Varman <89729737+Surya-Varman@users.noreply.github.com>
2021-09-28 18:40:13 +0530

    Update Lot.java

--- Lot.java
+++ Lot.java
@@ -1,11 +1,25 @@
 
 public class Lot
 {
-    private final int dimensions[] ;
     private boolean is_full;
-    Lot(int dimensions[])
-    {
-        this.dimensions = dimensions;
-        is_full = false;
+    Vehicle vehicle_in_lot;
+    Electric_Panel e;
+    public void assign_vehicle(Vehicle v){
+        this.vehicle_in_lot=v;
+        if(vehicle_in_lot.getSlot_id().charAt(3)=='E')
+            e = new Electric_Panel();
+    }
+    Lot(){
+        is_full=false;
+
+    }
+
+    public void setIs_full(){
+        if(is_full==false)
+            is_full=true;
+    }
+
+    public boolean isIs_full() {
+        return is_full;
     }
 }
